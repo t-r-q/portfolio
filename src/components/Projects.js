b88400a39d19efc3b458dff86636aa3d649e7456
trq <trrqjms@gmail.com>
2023-11-29 13:55:27 +0100

    add new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,34 @@ import Project from './Myproject';
 import monytracking from "../png/MoneyTracking.png";
 import munami from "../png/MunamiiCakery.png";
 import todo from "../png/todo.png";
+import tst3 from "../png/web3.png";
+import tst6 from "../png/web6.png";
 
 
 const MY_Projects = [
+  {
+    id: 'e5',
+    title: 'Project Prediction testcases with AI in Java',
+    detail: 'An application to predict and determine priority of test cases based on test behavior using Mutation Testing & Regression Testing.',
+    langaug: 'Java Languages',
+    linkIt: 'https://github.com/t-r-q/ReadMatrixPro',
+   img_path: tst6
+  },
+  {
+    id: 'e6',
+    title: 'Project Share Drawing Board in Reactive Rxjava',
+    detail: ' The main objective of this project is to create a collaborative drawing board using Java 2D. This board will allow multiple users to draw different shapes simultaneously. Key Features: Drawing Shapes, Collaboration, Group Management and Server Connection for group',
+    langaug: 'Java, Swing, and Reactive Rxjava Languages',
+    linkIt: 'https://github.com/t-r-q/DrawWHIT_Java',
+   img_path: tst3
+  }, {
+    id: 'e4',
+    title: 'Project Mobile Dailer Android',
+    detail: 'The Dialer app for Android is a software application that enables users to place telephone calls from their Android devices. Key features: Functionality: The Dialer app allows users to make phone calls, manage contacts, and view call history. It also provides features such as speed-dial, group calling. Customization: default sound for numbers with many languages. Variety: Obtaining permissions to determine the callers location.',
+    langaug: 'Android, Java Languages',
+    linkIt: 'https://github.com/t-r-q/androidDailer',
+   img_path: tst3
+  },
   {
     id: 'e1',
     title: 'Project MunamiiCakery Website',
@@ -28,7 +53,7 @@ const MY_Projects = [
     langaug: 'C# language',
     linkIt: "https://github.com/t-r-q/TodoListApp",
     img_path: todo
-  },
+  },  
 ];
 
 
